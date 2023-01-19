ca29ffa52cc754ef2e8dcac827999958fdc88d66
Zamira <keldibaevaz@gmail.com>
2023-01-19 21:50:15 +0300

    refactoring some mistakes2 and changes dependencies

--- src/main/java/com/calculator/calculatorforhome/domain/buildmodel/Bathroom.java
+++ src/main/java/com/calculator/calculatorforhome/domain/buildmodel/Bathroom.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.List;
 
 /**
  * Ванная
  */
-
 @Getter
 @Setter
 @NoArgsConstructor
