6cd5eb2490e452c30225f8e16b0be629f1e30bd6
Zamira <keldibaevaz@gmail.com>
2023-01-19 17:28:21 +0300

    Initial commit

---
+++ src/main/java/com/calculator/calculatorforhome/CalculatorForHomeApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.calculatorforhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorForHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorForHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calculator/calculatorforhome/CalculatorForHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.calculatorforhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorForHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
